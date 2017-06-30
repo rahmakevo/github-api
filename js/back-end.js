003c7f8aa4338074807dcf45a258df15fd4d83e3
rahmakevo <siundu344@gmail.com>
2017-06-30 19:24:06 +0300

    add file without heroku

--- js/back-end.js
+++ js/back-end.js
@@ -1,11 +1,23 @@
 function userAccount() {
   //handle user info
-};
+}
 
 var apiKey = ('./../.envfile').apiKey;
 
 userAccount.prototype.getUserInfo = function(account){
   $.get('https://api.github.com/users/' +account+ '?access_token' + apiKey).then(function(response) {
+  $.ajax({
+    url: 'https://api.github.com/users/' +account+ '/repos',
+    sort: 'created: asc',
+    per_page: 5
+  }).done(function(repos) {
+    $.each(repos, function(index, repo) {
+      console.log(repo);
+      $('#repos').append('<ul><li>' +repo.name+' '+repo.created_at+' '+repo.language +'</li></ul>');
+      // $('#date').text(repo.created_at);
+      // $('#langs').text(repo.language);
+    });
+  });
     console.log(response);
     $('#avatar').html('<img src="' + response.avatar_url + '">');
   if (response.avatar_url == null) {
@@ -32,14 +44,14 @@ userAccount.prototype.getUserInfo = function(account){
         response.name = "Input correct user info";
       }
       $('#followed').text(response.following);
-      if (response.public_repos == null) {
-        response.name = "Input correct user info";
-      }
-      $('#repos').text(response.public_repos);
+      // if (response.public_repos == null) {
+      //   response.name = "Input correct user info";
+      // }
+      // $('#repos').text(response.public_repos);
       if (response.repos_url == null) {
         response.name = "Input correct user info";
       }
-      $('#repo_url').text(response.repos_url)
+      $('#repo_url').text(response.repos_url);
 
   }).fail(function(error) {
     console.log(error.responseJSON.message);
