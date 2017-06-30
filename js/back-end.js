0a5bcc1106134f3a90e4a5b140e8894a00c9ddf1
rahmakevo <siundu344@gmail.com>
2017-06-30 16:45:57 +0300

    push to git

--- js/back-end.js
+++ js/back-end.js
@@ -1,5 +1,47 @@
 function userAccount() {
-  //api call
+  //handle user info
 };
 
+var apiKey = ('./../.envfile').apiKey;
+
+userAccount.prototype.getUserInfo = function(account){
+  $.get('https://api.github.com/users/' +account+ '?access_token' + apiKey).then(function(response) {
+    console.log(response);
+    $('#avatar').html('<img src="' + response.avatar_url + '">');
+  if (response.avatar_url == null) {
+    response.name = "Input correct user info";
+  }
+  $('#avatarurl').text(response.avatar_url);
+    if (response.login == null) {
+      response.name = "Input correct user info";
+    }
+    $('#full_name').text(response.login);
+    if (response.name == null) {
+      response.name = "Input correct user info";
+    }
+      $('#ghapidata').text(response.name);
+      if (response.html_url == null) {
+        response.name = "Input correct user info";
+      }
+      $('#profile_url').text(response.html_url);
+      if (response.followers == null) {
+        response.name = "Input correct user info";
+      }
+      $('#follow').text(response.followers);
+      if (response.following == null) {
+        response.name = "Input correct user info";
+      }
+      $('#followed').text(response.following);
+
+      // var repos = '';
+      // $.get('https://api.github.com/users/repos' + account +)
+      if (response.public_repos == null) {
+        response.name = "Input correct user info";
+      }
+      $('#repos').text(response.public_repos);
+
+  }).fail(function(error) {
+    console.log(error.responseJSON.message);
+  });
+}
 exports.userAccountModule = userAccount;
--- js/front-end.js
+++ js/front-end.js
@@ -1,20 +1,13 @@
 var userAccount = require('./../js/back-end.js').userAccountModule;
 
-var apiKey = ('./../.envfile').userAccountModule;
-
 $(document).ready(function() {
   $('form#interface').submit(function(event) {
     event.preventDefault();
 
     var account = $('#ghusername').val();
     console.log(account);
-    $.get('https://api.github.com/users/daneden?access_token' + apiKey).then(function(response) {
-      console.log(response.login);
-    }).fail(function(error) {
-      console.log(error.responseJSON.message);
-    })
-    var userAccountObject = new userAccount();
-    var output = userAccountObject.getUserName(account);
-    console.log(output);
+    var userAccountObject = new userAccount(account);
+    userAccountObject.getUserInfo(account);
+    console.log(userAccountObject);
   });
 });
