ea4e6deef81ca9d82820dc08ab98f08fa72c0b13
rahmakevo <siundu344@gmail.com>
2017-06-30 11:48:27 +0300

    making changes to project

--- js/front-end.js
+++ js/front-end.js
@@ -1,6 +1,6 @@
 var userAccount = require('./../js/back-end.js').userAccountModule;
 
-var apiKey = require('./../.envfile').apiKey;
+var apiKey = ('./../.envfile').userAccountModule;
 
 $(document).ready(function() {
   $('form#interface').submit(function(event) {
@@ -8,6 +8,11 @@ $(document).ready(function() {
 
     var account = $('#ghusername').val();
     console.log(account);
+    $.get('https://api.github.com/users/daneden?access_token' + apiKey).then(function(response) {
+      console.log(response);
+    }).fail(function(error) {
+      console.log(error.responseJSON.message);
+    })
     var userAccountObject = new userAccount();
     var output = userAccountObject.getUserName(account);
     console.log(output);
